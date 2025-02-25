ba436cc724f3318cc97aceea37c6ca39ef9318b0
LazyPrxncess <nordgren.cameron@gmail.com>
2025-02-25 13:18:06 -0700

    Added pycache to ignore, imported constants into main, added screen dimentions

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
